4dba0a904575bd3a7c20dc8dc683bda544def415
Lucas Lima de Oliveira <lloliveira@inf.ufrgs.br>
2021-07-15 14:46:50 -0300

    Updated volunteers section

--- src/components/volunteers/index.jsx
+++ src/components/volunteers/index.jsx
@@ -15,73 +15,37 @@ function Volunteers() {
       </div>
 
       <div className="container">
-        <div className="row">
+        <div className="row d-flex justify-content-center">
           <div className="col-md-4 mt-5">
             <div className="fh5co-testimonial text-center animate-box">
               <figure>
-                <img src="images/foto_gerson.jpg" alt="Voluntário Gerson" title="Gerson" />
+                <img src="images/foto_andrea.jpg" alt="Voluntária Andrea" title="Andrea" />
               </figure>
               <blockquote>
-                <p>
-                  Tem experiências que simplesmente podem mudar ou dar um verdadeiro sentido pra vida de uma pessoa. 
-                  Um desses momentos eu vivi em 2017 quando viajei minha primeira vez para Moçambique. Vim a convite de 
-                  meu colega e irmão da vida Gabriel, para ajudar na construção de poços artesianos em aldeias sem acesso à 
-                  água potável. Um desafio e tanto, mas como sempre foi um dos meus sonhos vir pra África e atuar socialmente, 
-                  aceitei de imediato. Chegando em Moçambique tive o choque de realidade no momento que atravessei a fronteira, 
-                  realmente comecei a entender a importância do que iríamos fazer para aquelas pessoas. Logo após a inauguração 
-                  do primeiro poço, um grande número de pessoas começou a dançar ao redor agradecendo imensamente e com uma 
-                  felicidade genuína nos rostos. Entendi então qual era o verdadeiro sentido e meu rumo na vida dali pra frente. 
-                  Voltei ao Brasil, me formei na faculdade de Engenharia Civil e junto com o Gabriel e o Rômulo, mais um irmão 
-                  que o universo me apresentou, voltamos pra Moçambique pra trabalharmos socialmente. Resolvemos fundar o Lwandi 
-                  surf que atende crianças da cidade de Ponta do Ouro trabalhando com práticas de surf, yoga, natação, além de 
-                  aulas de Inglês e acompanhamento escolar.
+                <p className="text-center">
+                  "Sou grata pelo meu tempo como voluntária na Lwandi Surf. É uma organização que se orgulha de tudo o que faz, 
+                  ao mesmo tempo em que realiza uma contribuição significativa para a comunidade da Ponta do Ouro. Adorei fazer 
+                  parte dos programas de ensino de inglês e Yoga. A equipe da Lwandi é apaixonada pelo o que faz e é formada por 
+                  dedicados líderes comunitários e me sinto muito orgulhosa de ter trabalhado para esta ONG!"
                 </p>
               </blockquote>
-              <span>Gerson Severo da Trindade</span>
+              <span>Andrea Williams</span>
             </div>
           </div>
           <div className="col-md-4 mt-5">
             <div className="fh5co-testimonial text-center animate-box">
               <figure>
-                <img src="images/foto_gabriel.jpg" alt="Voluntário Gabriel" title="Gabriel" />
+                <img src="images/foto_michela.jpg" alt="Voluntário Michela" title="Michela" />
               </figure>
               <blockquote>
-                <p>
-                  Em 2017, quando vim a Moçambique com mais dois amigos ajudar em um projeto social de distribuição de água potável 
-                  por 40 dias, tive o rumo da minha vida mudado. O que mais impacta ao entrar nas comunidades moçambicanas é a falta 
-                  de higiene, saneamento básico e energia, para ter noção, muitas comunidades aqui usam o lixo como a 'base' das casas, 
-                  que geralmente têm no máximo 20 m². Vimos que com o pouco conhecimento e recursos que tínhamos podíamos contribuir 
-                  para o desenvolvimento social de Moçambique, e então, decidimos voltar um dia e montar um projeto a longo prazo. 
-                  Junto com o Gerson e o Rômulo, amigos que se tornaram irmãos para a vida, começamos em Outubro de 2019 o Lwandi Surf, 
-                  projeto que visa dar oportunidade para os jovens locais na área da educação, esporte e fornecer a eles um local 
-                  onde se sintam seguros e confortáveis. Ainda não temos  previsão de voltar ao Brasil pois aqui nos sentimos em casa. 
-                  Moçambicanos e brasileiros se dão muito bem e sempre fomos bem recebidos em todas as comunidades que passamos, mas o 
-                  que mais me impressiona é a alegria e sorriso no rosto que eles tem mesmo com todas as adversidades que passam, eles 
-                  têm um espírito coletivo muito forte, dançam e cantam bastante e quem mais aprende no final das contas é a gente.
+                <p className="text-center">
+                  "Ser voluntária da Lwandi Surf foi uma experiência incrível, e sou muito grata pelo meu tempo com eles. 
+                  Gabriel e Rômulo são apaixonados pelo que fazem, sua energia e ética de trabalho também refletem isso. 
+                  Adorei me envolver com as crianças através da yoga, aulas de inglês, atividades em grupos e aulas de natação, 
+                  além de aprender mais sobre a cultura e terapia do surfe. Obrigado por esta oportunidade."
                 </p>
               </blockquote>
-              <span>Gabriel Fonseca Carrião de Freitas</span>
-            </div>
-          </div>
-          <div className="col-md-4 mt-5">
-            <div className="fh5co-testimonial text-center animate-box">
-              <figure>
-                <img src="images/foto_romulo.jpg" alt="Voluntário Rômulo" title="Rômulo"/>
-              </figure>
-              <blockquote>
-                <p>
-                  "Sempre me vejo um dia no continente africano" Assim como viajar pelo mundo, essa era uma das frases que muitas 
-                  vezes surgiam na minha cabeça. Quando estava para me formar, no final de 2019,  recebi um convite inesperado de 
-                  um amigo meu de longa data, Gabriel. Não pensei duas vezes em aceitar o convite de ir para Moçambique com dois 
-                  grandes irmãos, tudo isso graças ao apoio dos meus pais também. A grande diferença é que eles já haviam visitado 
-                  Moçambique em 2017, logo, eu era o novato do time, escutando muitas histórias e só imaginando a tão esperada viagem. 
-                  Moçambique é muito mais do que eu esperava, em diversos sentidos.É difícil de explicar. Logo de cara, já percebi que 
-                  aquela minha frase não fazia o menor sentido, África é gigante e extremamente diversa, inclusive dentro dos países. 
-                  Com o projeto Lwandi Surf, estamos completando o nosso objetivo de trabalhar socialmente nesse país e impactar a vida 
-                  de muitos jovens. Estamos recém no início, mas temos planos gigantes para eles. Vai ser demais!
-                </p>
-              </blockquote>
-              <span>Rômulo de Lima de Oliveira</span>
+              <span>Michela Gordan</span>
             </div>
           </div>
         </div>
