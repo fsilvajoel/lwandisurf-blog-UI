aadabc7ba4524bb19fa605cd4024826caa11069e
JOEL FERREIRA DA SILVA <joel.silva@afya.com.br>
2022-11-18 15:37:22 -0300

    finish adjust translate component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,50 +1,66 @@
 import React from "react";
 import TranslateMenu from "./translateMenu";
-import useTranslation from "../i18n/translator"
+import useTranslation from "../i18n/translator";
 
+const isDestkop = () => {
+  var width = window.innerWidth;
+  if (width > 1024) {
+    return true;
+  }
+  return false;
+};
 function Header({ page = "home" }) {
+  console.log("desk?", isDestkop());
   return (
     <header id="fh5co-header-section" className="sticky-banner">
-        <div className="container">
-            {/* { this.props.children } */}
-            <div className="nav-header">
-                <a href="#" className="js-fh5co-nav-toggle fh5co-nav-toggle dark">
-                    <i></i>
+      <div className="container">
+        <div className="nav-header">
+          <a className="js-fh5co-nav-toggle fh5co-nav-toggle dark">
+            <i></i>
+          </a>
+          <h1 id="fh5co-logo">
+            <a href="/" title="Home">
+              <img src="../images/logo.png" alt="logo" title="logo" />
+            </a>
+            {!isDestkop() && <TranslateMenu />}
+          </h1>
+          <nav id="fh5co-menu-wrap" role="navigation">
+            <ul className="sf-menu" id="fh5co-primary-menu">
+              <li className={`${page === "home" ? "active" : ""}`}>
+                <a href="/" title="Home">
+                  Home
                 </a>
-                <h1 id="fh5co-logo">
-                    <a href="/" title="Home">
-                        <img src="../images/logo.png" alt="logo" title="logo" />
-                    </a>
-                </h1>
-                {/* <!-- START #fh5co-menu-wrap --> */}
-                <nav id="fh5co-menu-wrap" role="navigation">
-                    <ul className="sf-menu" id="fh5co-primary-menu">
-                        <li className={`${page === "home" ? "active" : ""}`}>
-                            <a href="/" title="Home">Home</a>
-                        </li>
-                        <li className={`${page === "project" ? "active" : ""}`}>
-                            <a href="/project" title="Projeto">{ useTranslation("topMenu.project") }</a>
-                        </li>
-                        <li className={`${page === "blog" ? "active" : ""}`}>
-                            <a href="/blog" title="Blog">Blog</a>
-                        </li>
-                        <li className={`${page === "gallery" ? "active" : ""}`}>
-                            <a href="/gallery" title="Galeria">{ useTranslation("topMenu.gallery") }</a>
-                        </li>
-                        <li className={`${page === "about" ? "active" : ""}`}>
-                            <a href="/about" title="Sobre nós">{ useTranslation("topMenu.about") }</a>
-                        </li>
-                        <li className={`${page === "contact" ? "active" : ""}`}>
-                            <a href="/contact" title="Contato">{ useTranslation("topMenu.contact") }</a>
-                        </li>
-                        <li >
-                              <TranslateMenu/>
-                        </li>
-                    </ul>
-                </nav>
-             
-            </div>
+              </li>
+              <li className={`${page === "project" ? "active" : ""}`}>
+                <a href="/project" title="Projeto">
+                  {useTranslation("topMenu.project")}
+                </a>
+              </li>
+              <li className={`${page === "blog" ? "active" : ""}`}>
+                <a href="/blog" title="Blog">
+                  Blog
+                </a>
+              </li>
+              <li className={`${page === "gallery" ? "active" : ""}`}>
+                <a href="/gallery" title="Galeria">
+                  {useTranslation("topMenu.gallery")}
+                </a>
+              </li>
+              <li className={`${page === "about" ? "active" : ""}`}>
+                <a href="/about" title="Sobre nós">
+                  {useTranslation("topMenu.about")}
+                </a>
+              </li>
+              <li className={`${page === "contact" ? "active" : ""}`}>
+                <a href="/contact" title="Contato">
+                  {useTranslation("topMenu.contact")}
+                </a>
+              </li>
+              <li>{isDestkop() && <TranslateMenu />}</li>
+            </ul>
+          </nav>
         </div>
+      </div>
     </header>
   );
 }
--- src/components/translateMenu/index.js
+++ src/components/translateMenu/index.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import { useTranslation } from "react-i18next";
 const TranslateMenu = () => {
-  const { i18n } = useTranslation()
+  const { i18n } = useTranslation();
 
   function handleChangeLanguage(language) {
-    i18n.changeLanguage(language)
+    i18n.changeLanguage(language);
   }
   return (
-    <div className="flags-container" style={{padding: '12px 0'}}>
-      <button class="br" onClick={() => handleChangeLanguage('pt-BR')}></button>
-      {" "}
-      <button class="us" onClick={() => handleChangeLanguage('en-US')}></button>
+    <div className="flags-container" style={{ padding: "12px 0" }}>
+      <button class="br" onClick={() => handleChangeLanguage("pt-BR")}></button>{" "}
+      <button class="us" onClick={() => handleChangeLanguage("en-US")}></button>
     </div>
-  )
-}
+  );
+};
 
 export default TranslateMenu;
